1caf75ffdbe352f7d2516d2afc073d281aee40d3
nymeria41 <113991279+nymeria41@users.noreply.github.com>
2024-10-05 00:04:16 +0200

    First commit angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { VoitureListComponent } from './voiture-list/voiture-list.component';
+import { AddVoitureComponent } from './add-voiture/add-voiture.component';
+import { ModifyVoitureComponent } from './modify-voiture/modify-voiture.component';
+import { DetailsVoitureComponent } from './details-voiture/details-voiture.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'voitures', component: VoitureListComponent },
+  { path: 'add', component: AddVoitureComponent },
+  { path: 'edit/:id', component: ModifyVoitureComponent },
+  { path: 'voitures/:id', component: DetailsVoitureComponent },
+  { path: '', redirectTo: '/voitures', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
